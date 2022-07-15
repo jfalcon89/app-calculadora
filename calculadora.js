89ab68fed8bba95c0092939f0abc685cd07624bc
jfalcon89 <jfalcon89@hotmail.com>
2022-07-15 14:32:57 -0400

    first commit

---
+++ calculadora.js
@@ -0,0 +1,7 @@
+// const button7 = document.getElementById("button7").value
+// const textfield = document.getElementById("textfield")
+// const calculadora = document.body
+
+// button7.addEventListener("click", () => {
+//     document.calculadora.respuesta.value += '7'
+// })
